8c736998aa6590354391fe0ebde389561171a3f5
Nazano <antoninus.antoninus@me.com>
2021-06-02 15:44:26 +0200

    [ADD][main] ajout du main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    if(argc < 2)
+    {
+        std::cerr << "Le graphe est attendu en argument" << std::endl;
+        return 1;
+    }
+    std::cout << "Not implemented";
+    return 0;
+}
